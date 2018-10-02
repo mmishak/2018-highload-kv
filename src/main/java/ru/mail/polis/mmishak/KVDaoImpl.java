58d4a1a90c0fd1104d530e5937bcb487fe3cdbe1
Mikhail Kozlov <mmishak.ya@gmail.com>
2018-10-02 15:46:19 +0300

    implement KVDao

--- src/main/java/ru/mail/polis/mmishak/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/mmishak/KVDaoImpl.java
@@ -3,28 +3,48 @@ package ru.mail.polis.mmishak;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.NoSuchElementException;
 
 public class KVDaoImpl implements KVDao {
+
+    private final File storageDirectory;
+
+    public KVDaoImpl(File storage) {
+        this.storageDirectory = storage;
+    }
+
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        return new byte[0];
+        File file = new File(storageDirectory.getAbsolutePath() + new String(key));
+        if (!file.exists())
+            throw new NoSuchElementException("File with key'" + new String(key) + "' not exits");
+        return Files.readAllBytes(file.toPath());
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-
+        File file = new File(storageDirectory.getAbsolutePath() + new String(key));
+        if (!file.exists()) {
+            final boolean success = file.createNewFile();
+            if (!success)
+                throw new IOException();
+        }
+        Files.write(file.toPath(), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-
+        File file = new File(storageDirectory.getAbsolutePath() + new String(key));
+        final boolean success = !file.exists() || file.delete();
+        if (!success)
+            throw new IOException();
     }
 
     @Override
     public void close() throws IOException {
-
     }
 }
